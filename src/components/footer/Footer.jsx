cf95c7222f85c4a488720426793e05971634d7ae
HUDEVBR <forhudson@hotmail.com>
2025-08-17 19:19:25 -0300

    Adicionando estilos do modulo do CSS

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  console.log("Olá")
+  return (
+    <div>
+      <div>©2025 HUDEVBR. Todos os direitos reservados</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Footer
